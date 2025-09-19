0a5f668c08539230bff717c76aae8e997c2cffc9
liuyao <liuyao@zhito.com>
2025-09-19 15:23:35 +0800

    add mcp demo
    Change-Id: I02eb56581e7a3a1e9e81a8d7d08329a9539d4d62

---
+++ 12_Agent/hf_course/02_mcp_server.py
@@ -0,0 +1,30 @@
+from mcp.server.fastmcp import FastMCP
+import os
+
+os.environ["DANGEROUSLY_OMIT_AUTH"] = "true"
+# Create an MCP server
+mcp = FastMCP("Weather Service")
+
+# Tool implementation
+@mcp.tool()
+def get_weather(location: str) -> str:
+    """Get the current weather for a specified location."""
+    return f"Weather in {location}: Sunny, 72°F"
+
+# Resource implementation
+@mcp.resource("weather://{location}")
+def weather_resource(location: str) -> str:
+    """Provide weather data as a resource."""
+    return f"Weather data for {location}: Sunny, 72°F"
+
+# Prompt implementation
+@mcp.prompt()
+def weather_report(location: str) -> str:
+    """Create a weather report prompt."""
+    return f"""You are a weather reporter. Weather report for {location}?"""
+
+
+# Run the server
+# tip: install uv for connection
+if __name__ == "__main__":
+    mcp.run(transport="sse")
