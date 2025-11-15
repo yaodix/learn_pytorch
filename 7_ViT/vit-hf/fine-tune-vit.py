de764d24fe94a32fe78d4eaeb0a26024e2f87915
yaodix <liuyao199111@163.com>
2025-11-15 12:22:14 +0800

    add finetune vit demo

--- 7_ViT/vit-hf/fine-tune-vit.py
+++ 7_ViT/vit-hf/fine-tune-vit.py
@@ -1,3 +1,105 @@
 # Fine-Tune ViT for Image Classification with 🤗 Transformers
 # https://huggingface.co/blog/fine-tune-vit
 
+from datasets import load_dataset
+import random
+from PIL import ImageDraw, ImageFont, Image
+import torch
+from transformers import ViTImageProcessor, ViTForImageClassification, TrainingArguments, Trainer
+import numpy as np
+from evaluate import load
+
+def show_examples(ds, seed: int = 1234, examples_per_class: int = 3, size=(350, 350)):
+
+    w, h = size
+    labels = ds['train'].features['labels'].names
+    labels_feature = ds['train'].features['labels']
+    print(f"Labels feature: {labels_feature}")
+    print(f"Available attributes: {[attr for attr in dir(labels_feature) if not attr.startswith('_')]}")
+    grid = Image.new('RGB', size=(examples_per_class * w, len(labels) * h))
+    draw = ImageDraw.Draw(grid)
+    font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationMono-Bold.ttf", 24)
+
+    for label_id, label in enumerate(labels):
+
+        # Filter the dataset by a single label, shuffle it, and grab a few samples
+        ds_slice = ds['train'].filter(lambda ex: ex['labels'] == label_id).shuffle(seed).select(range(examples_per_class))
+
+        # Plot this label's examples along a row
+        for i, example in enumerate(ds_slice):
+            image = example['image']
+            idx = examples_per_class * label_id + i
+            box = (idx % examples_per_class * w, idx // examples_per_class * h)
+            grid.paste(image.resize(size), box=box)
+            draw.text(box, label, (255, 255, 255), font=font)
+
+    return grid
+  
+def transform(example_batch):
+    # Take a list of PIL images and turn them to pixel values
+    inputs = processor([x for x in example_batch['image']], return_tensors='pt')
+
+    # Don't forget to include the labels!
+    inputs['labels'] = example_batch['labels']
+    return inputs
+  
+def collate_fn(batch):
+    return {
+        'pixel_values': torch.stack([x['pixel_values'] for x in batch]),
+        'labels': torch.tensor([x['labels'] for x in batch])
+    }
+    
+metric = load("accuracy")
+def compute_metrics(p):
+    return metric.compute(predictions=np.argmax(p.predictions, axis=1), references=p.label_ids)
+
+training_args = TrainingArguments(
+  output_dir="./vit-base-beans",
+  per_device_train_batch_size=16,
+  eval_strategy="steps",
+  num_train_epochs=4,
+  fp16=True,
+  save_steps=100,
+  eval_steps=100,
+  logging_steps=10,
+  learning_rate=2e-4,
+  save_total_limit=2,
+  remove_unused_columns=False,
+  push_to_hub=False,
+  report_to='tensorboard',
+  load_best_model_at_end=True,
+)
+
+if __name__ == '__main__':
+  ds = load_dataset('beans')
+  prepared_ds = ds.with_transform(transform)
+
+  model_name_or_path = 'google/vit-base-patch16-224-in21k'
+  processor = ViTImageProcessor.from_pretrained(model_name_or_path)
+  
+  labels = ds['train'].features['labels'].names
+  model = ViTForImageClassification.from_pretrained(
+    model_name_or_path,
+    num_labels=len(labels),
+    id2label={str(i): c for i, c in enumerate(labels)},
+    label2id={c: str(i) for i, c in enumerate(labels)}
+  )
+  trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=collate_fn,
+    compute_metrics=compute_metrics,
+    train_dataset=prepared_ds["train"],
+    eval_dataset=prepared_ds["validation"],
+    tokenizer=processor,
+  )
+  train_results = trainer.train()
+  
+  trainer.save_model()
+  trainer.log_metrics("train", train_results.metrics)
+  trainer.save_metrics("train", train_results.metrics)
+  trainer.save_state()
+
+  metrics = trainer.evaluate(prepared_ds['validation'])
+  trainer.log_metrics("eval", metrics)
+  trainer.save_metrics("eval", metrics)
